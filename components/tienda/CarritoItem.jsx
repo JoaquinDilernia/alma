2b3798ff4d9903ef103c0f94ee31ff99578e1366
dilerr <jdilernia99@gmail.com>
2026-07-24 10:03:23 -0300

    feat: cart item shows garnishes and operates by lineId

--- components/tienda/CarritoItem.jsx
+++ components/tienda/CarritoItem.jsx
@@ -1,23 +1,29 @@
 "use client";
 
 import { useCart } from "@/lib/CartProvider";
+import { cartLineId } from "@/lib/cart";
 import styles from "./CarritoItem.module.css";
 
 export default function CarritoItem({ item }) {
   const { updateCartQuantity, removeFromCart } = useCart();
+  const lineId = cartLineId(item);
+  const guarniciones = item.guarniciones || [];
 
   return (
     <div className={styles.row}>
-      <span className={styles.nombre}>{item.nombre}</span>
+      <div className={styles.info}>
+        <span className={styles.nombre}>{item.nombre}</span>
+        {guarniciones.length > 0 && <span className={styles.guarniciones}>{guarniciones.join(", ")}</span>}
+      </div>
       <span>${item.precio}</span>
       <input
         type="number"
         min={1}
         value={item.cantidad}
-        onChange={(e) => updateCartQuantity(item.productoId, Number(e.target.value) || 1)}
+        onChange={(e) => updateCartQuantity(lineId, Number(e.target.value) || 1)}
         className={styles.cantidad}
       />
-      <button type="button" className={styles.quitar} onClick={() => removeFromCart(item.productoId)}>
+      <button type="button" className={styles.quitar} onClick={() => removeFromCart(lineId)}>
         Quitar
       </button>
     </div>
